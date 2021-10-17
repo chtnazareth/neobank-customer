d21c8f6f7280a56b8a7ed622a6a2797c9c29174c
Claudio Nazareth <chtnazareth@gmail.com>
2021-10-17 18:42:02 +0200

    Initial configuration

--- src/main/java/de/neobank/customer/NeoBankCustomerApplication.java
+++ src/main/java/de/neobank/customer/NeoBankCustomerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NeobankCustomerApplication {
+public class NeoBankCustomerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NeobankCustomerApplication.class, args);
+        SpringApplication.run(NeoBankCustomerApplication.class, args);
     }
 
 }
