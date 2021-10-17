ad0630116df0a08d02046b9c715c6d65e304ebee
Claudio Nazareth <chtnazareth@gmail.com>
2021-10-17 18:22:35 +0200

    Project creation

---
+++ src/main/java/de/neobank/customer/NeobankCustomerApplication.java
@@ -0,0 +1,13 @@
+package de.neobank.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeobankCustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeobankCustomerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neobank/customer/NeobankCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neobank.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeobankCustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
